427058ca02f39066a235ee3095efb4a76e3c4c09
italijancic <italijancic@outlook.com>
2023-04-19 18:05:28 -0300

    Add CLI prompt read line

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log('Hello world!')
+import { createInterface } from 'readline'
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+
+const readLineAsync = (msg) => {
+  return new Promise(resolve => {
+    readline.question(msg, userRes => {
+      resolve(userRes)
+    })
+  })
+}
+
+const myApp = async() => {
+
+  // Here: students code
+  const userRes = await readLineAsync('How are you? ')
+  readline.close()
+  console.log('Your response was: ' + userRes + ' — Thanks!')
+
+
+}
+
+myApp()
